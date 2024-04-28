5434c3864266f88d8593e13e40cccf19415c2541
Devraj-prasad07 <prasaddeb95@gmail.com>
2024-04-28 17:34:12 +0530

    DAY 3

---
+++ 3.Count_Occurences_Of_Character.js
@@ -0,0 +1,15 @@
+//*------------------------------------------------------
+// Interview Question : Count Occurences of Character
+//*------------------------------------------------------
+
+//! Task:
+//? Write a function called countChar that takes two parameters: a string and a character to count. The function should return the number of times the specified character appears in the string.
+
+// todo Constraints
+//? The function should be case-sensitive.
+//? The function should handle both lowercase and uppercase characters.
+//? The character parameter can be printable ASCII character function should accept any character that is part of the ASCII character set and is printable.
+
+const countChar = (string,charCount) => {
+    
+}
