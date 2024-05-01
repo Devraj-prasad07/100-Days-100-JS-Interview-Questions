22137be448fdfbb1d1afb0ce48442f4290eb3b73
Devraj-prasad07 <prasaddeb95@gmail.com>
2024-05-02 00:16:57 +0530

    DAY 3

--- 3.Count_Occurences_Of_Character.js
+++ 3.Count_Occurences_Of_Character.js
@@ -10,17 +10,29 @@
 //? The function should handle both lowercase and uppercase characters.
 //? The character parameter can be printable ASCII character function should accept any character that is part of the ASCII character set and is printable.
 
-const countChar = (string,charCount) => {
-    string = string.toLowerCase();
-    charCount = charCount.toLowerCase();
+const countChar = (string, charCount) => {
+  string = string.toLowerCase();
+  charCount = charCount.toLowerCase();
 
-    totalChar = string.split("").reduce((previous,current) => {
-        if (current === charCount){
-            previous++;
-        }
-        return previous
-    },0);
-    return totalChar;
-}
+  totalChar = string.split("").reduce((previous, current) => {
+    if (current === charCount) {
+      previous++;
+    }
+    return previous;
+  }, 0);
+  return totalChar;
+};
 
-console.log(countChar("MissIssipi","I"));
+console.log(countChar("MissIssipi", "I"));
+
+// How Reduce Fn Works
+let arr = ["a", "b", "c", "d", "e", "f", "e", "e", "e"];
+let char = arr.reduce((prev, curr) => {
+  console.log(`curr : ${curr}`);
+  if (curr === "e") {
+    prev++;
+    console.log(`prev 1 : ${prev}`);
+  }
+  return prev;
+}, 0);
+console.log(char);
